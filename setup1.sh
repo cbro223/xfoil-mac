d82c021f930895239b3a97219c35a9406554d9e2
Cory Brown <cory.brown@gpsit.co.nz>
2026-03-18 15:12:55 +1300

    Final setup script

---
+++ setup1.sh
@@ -0,0 +1,20 @@
+if [ -f "BEM.m" ]; then
+    echo ""
+else
+    echo "Please run this script in the same directory as the rest of the files"
+    exit
+fi
+
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+brew install gfortran
+brew install xquartz
+
+
+mv ./xfoil-mac/xfoil.m ./xfoil/xfoil.m
+
+sh setupxfoil.sh
+
